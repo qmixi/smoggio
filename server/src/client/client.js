b250a33dca8df9abcb5c19fa57e7d219c3ec8e25
Piotr Kumorek <piotr.kumi@gmail.com>
2018-12-05 20:54:51 +0100

    Add client routing

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,6 +4,12 @@ console.log('Hi there')
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './components/Home'
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes'
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>,
+    document.querySelector('#root')
+);

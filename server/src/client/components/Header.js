70f2206c0d977f49a10dd9ce725952c399152f9a
Piotr Kumorek <piotr.kumi@gmail.com>
2018-12-16 14:06:48 +0100

    remove bundle files from repo

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ auth }) => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <Link to="/" className="brand-logo">SSR APP</Link>
+                <Link to="/" className="brand-logo">SSR APP!</Link>
                 <ul className="right">
                     <li><Link to="/users">Users</Link></li>
                     <li><Link to="/admins">Admins</Link></li>

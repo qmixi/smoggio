7718ff2037ccc2bc0fc618b6e6b5c005fec75ddf
Piotr Kumorek <piotr.kumi@gmail.com>
2018-12-16 14:17:17 +0100

    Migrate react version to 16.7 alpha.02 & add useState example

--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Home = () => {
+    const [count, setCount] = useState(2);
     return (
         <div className="center-align" style={{ marginTop: '200px' }}>
-            <h3>Welcome!</h3>
-            <p>Check out my ssr app</p>            
+            <h3>Welcome!! Count: {count}</h3>
+            <p>Check out my ssr app</p>   
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     )
 }

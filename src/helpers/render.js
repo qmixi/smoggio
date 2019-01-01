c07d412a942dbc6d81bb6defb467997be6f7fdf3
Piotr Kumorek <piotr.kumi@gmail.com>
2019-01-01 19:52:55 +0100

    Add utf-8 support

--- src/helpers/render.js
+++ src/helpers/render.js
@@ -29,11 +29,12 @@ export default (req, state, context) => {
     return `
         <html>
             <head>
+                <meta charset="utf-8" />
                 ${helmet.title.toString()}
                 ${helmet.meta.toString()}
                 <link rel="stylesheet" href="/static/styles.css">
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-                <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:600" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:600" rel="stylesheet">                
             </head>
             <body>
                 <div id="root">${content}</div>
